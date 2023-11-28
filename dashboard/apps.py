cac0b38c52e905666db22cdde5b5c29019420d2f
mohammad maso <mohammadmasoudie@gmail.com>
2023-11-28 15:30:49 +0330

    pow

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -2,7 +2,7 @@ from django.contrib.admin.apps import AdminConfig
 
 
 class MyAdminConfig(AdminConfig):
-    default_site = "fapars1.admin.MyAdminSite"
+    default_site = "dashboard.admin.MyAdminSite"
     default_auto_field = "django.db.models.BigAutoField"
     name = "dashboard"
     verbose_name = "داشبورد"
