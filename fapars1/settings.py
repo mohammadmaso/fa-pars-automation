c94dada3d446efce523212089211844399d847a1
mohammad maso <mohammadmasoudie@gmail.com>
2023-11-28 12:46:30 +0330

    ojre

--- fapars1/settings.py
+++ fapars1/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME", "postgres"),
         "USER": os.environ.get("DB_USER", "postgres"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": "srv-captain--faparsdb",
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+        "HOST": os.environ.get("DB_HOST", "postgres"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
