97f3c7587190423826daeccd50a561879f5a56ed
mohammad maso <mohammadmasoudie@gmail.com>
2023-11-28 13:13:21 +0330

    print host name

--- fapars1/settings.py
+++ fapars1/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "fapars1.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+print(os.environ.get("DB_HOST"))
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
